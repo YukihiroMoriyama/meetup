b8fafe4126f45c51946dcd372c01580782b029f6
YukihiroMoriyama <m.y.like.music.10@gmail.com>
2016-06-22 12:00:43 +0900

    add EventDetailViewController

---
+++ TableSample/EventDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.swift
+//  TableSample
+//
+//  Created by yukihiro moriyama on 2016/06/22.
+//  Copyright © 2016年 YukihiroMoriyama. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+}
