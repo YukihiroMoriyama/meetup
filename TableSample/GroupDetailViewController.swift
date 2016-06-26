cecf454eef1c756813956e0bcf0cc3d36925c89d
YukihiroMoriyama <m.y.like.music.10@gmail.com>
2016-06-26 12:41:36 +0900

    MemberCell を追加して設定

---
+++ TableSample/GroupDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GroupDetailViewController.swift
+//  TableSample
+//
+//  Created by yukihiro moriyama on 2016/06/26.
+//  Copyright © 2016年 YukihiroMoriyama. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailViewController: UIViewController {
+
+    @IBOutlet var groupImageView: UIImageView!
+    @IBOutlet var MemberCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TableSample/MemberCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemberCell.swift
+//  TableSample
+//
+//  Created by yukihiro moriyama on 2016/06/26.
+//  Copyright © 2016年 YukihiroMoriyama. All rights reserved.
+//
+
+import UIKit
+
+class MemberCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+}
